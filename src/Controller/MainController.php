d450d6196f28b337f6175b7c1bd3984cca44176b
Gudauskas Julius <julgud@ktu.lt>
2019-11-17 22:01:50 +0200

    PushNr

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,9 +5,11 @@ use App\Form\UserType;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
-class MainController
+class MainController extends Controller
 {
     /**
      * @Route("/menu", name="menu")
@@ -22,4 +24,23 @@ class MainController
             array('form' => $form->createView())
         );*/
     }
+
+    /**
+     * @Route("/rooms", name="rooms")
+     */
+    public function loadrooms()
+    {
+        return $this
+        ->render('rooms.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+       /* $user = new User();
+        $form = $this->createForm(UserType::class, $user);*/
+        /*return $this->render(
+            'registration/register.html.twig',
+            array('form' => $form->createView())
+        );*/
+    }
+
+
 }
